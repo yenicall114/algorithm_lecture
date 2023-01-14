fc4c91b202971576a8550192c7a044004db5395e
yenicall <yenicall114@gmail.com>
2023-01-15 03:17:27 +0900

    s1-06

--- s1.js
+++ s1.js
@@ -129,7 +129,6 @@ console.log("lecture", solution(6));
 ▣ 입력예제 1
 5 3 7 11 2 15 17
 ▣ 출력예제 1 2
-*/
 
 const mySolution = (arr) => {
   return Math.min(...arr);
@@ -147,5 +146,56 @@ const solution = (n) => {
 };
 
 let arr = [5, 7, 1, 3, 2, 9, 11];
+console.log("me", mySolution(arr));
+console.log("lecture", solution(arr));
+
+
+6) 홀수
+7개의 자연수가 주어질 때, 이들 중 홀수인 자연수들을 모두 골라 그 합을 구하고, 고른 홀수들 중 최소값을 찾는 프로그램을 작성하세요.
+예를 들어, 7개의 자연수 12, 77, 38, 41, 53, 92, 85가 주어지면 이들 중 홀수는 77, 41, 53, 85이므로 그 합은
+77 + 41 + 53 + 85 = 256 이 되고,
+41 < 53 < 77 < 85 이므로 홀수들 중 최소값은 41이 된다.
+▣ 입력설명
+첫 번째 줄에 자연수 7개가 주어진다. 주어지는 자연수는 100보다 작다. 홀수가 한 개 이상 반드시 존재한다.
+▣ 출력설명
+첫째 줄에 홀수들의 합을 출력하고, 둘째 줄에 홀수들 중 최소값을 출력한다.
+▣ 입력예제 1
+12 77 38 41 53 92 85
+▣ 출력예제 1 
+256
+41
+*/
+
+const mySolution = (arr) => {
+  const result = [];
+
+  const odd = arr.filter((el) => el % 2 === 1);
+  const min = Math.min(...odd);
+
+  result.push(odd.reduce((a, c) => a + c));
+  result.push(min);
+
+  return result;
+};
+
+const solution = (n) => {
+  let answer = [];
+  let sum = 0,
+    min = Number.MAX_SAFE_INTEGER;
+
+  for (x of arr) {
+    if (x % 2 == 1) {
+      sum += x;
+      if (x < min) min = x;
+    }
+  }
+
+  answer.push(sum);
+  answer.push(min);
+
+  return answer;
+};
+
+let arr = [12, 77, 38, 41, 53, 92, 85];
 console.log("me", mySolution(arr));
 console.log("lecture", solution(arr));
