5bec852547f003c6f269682cac21dfbd065cf258
yenicall <yenicall114@gmail.com>
2023-01-15 02:22:11 +0900

    s1-03

--- s1.js
+++ s1.js
@@ -43,7 +43,6 @@ console.log("lecture", solution(numArr));
 ▣ 출력예제 1 YES
 ▣ 입력예제 1 13 33 17
 ▣ 출력예제 1 NO
-*/
 
 const mySolution = (a, b, c) => {
   const square = [a, b, c].sort((a, b) => a - b);
@@ -65,3 +64,30 @@ const solution = (a, b, c) => {
 
 console.log("me", mySolution(6, 7, 11));
 console.log("lecture", solution(13, 33, 17));
+*/
+
+/*
+연필 1 다스는 12자루입니다. 학생 1인당 연필을 1자루씩 나누어 준다고 할 때 N명이 학생수 를 입력하면 필요한 연필의 다스 수를 계산하는 프로그램을 작성하세요.
+▣ 입력설명
+첫 번째 줄에 1000 이하의 자연수 N이 입력된다.
+▣ 출력설명
+첫 번째 줄에 필요한 다스 수를 출력합니다.
+▣ 입력예제 1 25
+▣ 출력예제 1 3
+▣ 입력예제 2 178
+▣ 출력예제 2 15
+*/
+
+const mySolution = (n) => {
+  return Number.isInteger(n / 12) ? n / 12 : Math.ceil(n / 12);
+};
+
+const solution = (n) => {
+  let answer = Math.ceil(n / 12);
+  return answer;
+};
+
+// 엥 나 바보냐
+
+console.log("me", mySolution(25));
+console.log("lecture", solution(24));
