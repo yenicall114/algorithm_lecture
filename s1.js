dedc16b51d6e8ff31f09b402f2824133419fe6e1
yenicall <yenicall114@gmail.com>
2023-01-16 00:22:17 +0900

    s1-10

--- s1.js
+++ s1.js
@@ -316,7 +316,6 @@ console.log("lecture", solution(arr));
 첫 번째 줄에 바뀐 단어를 출력한다.
 ▣ 입력예제 1 BANANA
 ▣ 출력예제 1 B#N#N#
-*/
 
 const mySolution = (str) => {
   let arr = str.split("");
@@ -348,3 +347,36 @@ const solution = (str) => {
 let str = "BANANA";
 console.log("me", mySolution(str));
 console.log("lecture", solution(str));
+
+문제10) 문자 찾기
+한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+문자열의 길이는 100을 넘지 않습니다.
+▣ 입력설명
+첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+▣ 출력설명
+첫 줄에 해당 문자의 개수를 출력한다.
+▣ 입력예제 1 
+COMPUTERPROGRAMMING R
+▣ 출력예제 1 
+3
+*/
+
+const mySolution = (str, n) => {
+  let count = 0;
+
+  for (let x of str) {
+    if (x === n) count++;
+  }
+
+  return count;
+};
+
+const solution = (s, t) => {
+  let answer = s.split(t).length;
+  return answer - 1;
+};
+
+let str = "COMPUTERPROGRAMMING";
+let n = "R";
+console.log("me", mySolution(str, n));
+console.log("lecture", solution(str, n));
