cc768c8bf82f9c597e4cf3d3ea5f4fddb4f4221c
yenicall <yenicall114@gmail.com>
2023-01-16 00:50:11 +0900

    s1-12

--- s1.js
+++ s1.js
@@ -390,7 +390,6 @@ console.log("lecture", solution(str, n));
 KoreaTimeGood
 ▣ 출력예제 1 
 3
-*/
 
 const mySolution = (str) => {
   return [...str].filter((el) => el === el.toUpperCase()).length;
@@ -412,5 +411,45 @@ const solution = (s) => {
 };
 
 let str = "KoreaTimeGood";
+console.log("me", mySolution(str));
+console.log("lecture", solution(str));
+
+문제12) 대문자로 통일
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자로 모두 통일하여 문자열을 출력 하는 프로그램을 작성하세요.
+▣ 입력설명
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+▣ 출력설명
+첫 줄에 대문자로 통일된 문자열이 출력된다.
+▣ 입력예제 1 
+ItisTimeToStudy
+▣ 출력예제 1 
+ITISTIMETOSTUDY
+*/
+
+const mySolution = (str) => {
+  // let answer = "";
+
+  // for (let x of s) {
+  //   if (x.toLowerCase()) answer += x.toUpperCase();
+  //   else answer += x;
+  // }
+
+  // return answer;
+  return [...str].map((el) => el.toUpperCase()).join("");
+};
+
+const solution = (s) => {
+  let answer = "";
+
+  for (let x of s) {
+    let num = x.charCodeAt();
+    if (num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
+    else answer += x;
+  }
+
+  return answer;
+};
+
+let str = "ItisTimeToStudy";
 console.log("me", mySolution(str));
 console.log("lecture", solution(str));
