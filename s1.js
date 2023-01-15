6ee9527ed1ef241bc0afa89e8f49da03e4f2a0cb
yenicall <yenicall114@gmail.com>
2023-01-16 00:12:23 +0900

    s1-09

--- s1.js
+++ s1.js
@@ -264,7 +264,6 @@ console.log("lecture", solution(arr, n));
 20 7 23 19 10 15 25 8 13
 ▣ 출력예제 1
 20 7 23 19 10 8 13
-*/
 
 const mySolution = (arr) => {
   let num = [];
@@ -308,3 +307,44 @@ const solution = (arr) => {
 let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
 console.log("me", mySolution(arr));
 console.log("lecture", solution(arr));
+
+문제9) A를 #으로
+대문자로 이루어진 영어단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는 프로그램을 작성하세요.
+▣ 입력설명
+첫 번째 줄에 문자열이 입력된다.
+▣ 출력설명
+첫 번째 줄에 바뀐 단어를 출력한다.
+▣ 입력예제 1 BANANA
+▣ 출력예제 1 B#N#N#
+*/
+
+const mySolution = (str) => {
+  let arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "A") arr.splice(i, 1, "#");
+  }
+
+  return arr.join("");
+};
+
+const solution = (str) => {
+  // let answer = "";
+
+  // for (let x of str) {
+  //   if (x === "A") answer += "#";
+  //   else answer += x;
+  // }
+
+  // return answer;
+
+  let answer = str;
+
+  answer = answer.replace(/A/g, "#");
+
+  return answer;
+};
+
+let str = "BANANA";
+console.log("me", mySolution(str));
+console.log("lecture", solution(str));
