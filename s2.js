3c2851a9fad1d0d2b4551e741dbd419e4b5ce8eb
yenicall <yenicall114@gmail.com>
2023-01-17 00:53:06 +0900

    s2-05

--- s2.js
+++ s2.js
@@ -142,7 +142,6 @@ OX 문제는 맞거나 틀린 두 경우의 답을 가지는 문제를 말한다
 1011100110
 ▣ 출력예제 1 
 10
-*/
 
 const mySolution = (arr) => {
   let result = 0;
@@ -175,6 +174,41 @@ let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
 console.log("me", mySolution(arr));
 console.log("lecture", solution(arr));
 
-// 문5)
+// 문5) 등수구하기
+N(1<=N<=100)명의 학생의 국어점수가 입력되면 각 학생의 등수를 입력된 순서대로 출력하는 프로그램을 작성하세요.
+▣ 입력설명
+첫 줄에 N(3<=N<=1000)이 입력되고, 두 번째 줄에 국어점수를 의미하는 N개의 정수가 입력 된다. 같은 점수가 입력될 경우 높은 등수로 동일 처리한다. 즉 가장 높은 점수가 92점인데 92점이 3명 존재하면 1등이 3명이고 그 다음 학생은 4등이 된다.
+▣ 출력설명
+입력된 순서대로 등수를 출력한다.
+▣ 입력예제 1
+5
+87 89 92 100 76
+▣ 출력예제 1 
+4 3 2 1 5
+*/
+
+const mySolution = (arr) => {
+  const sort = arr.slice().sort((a, b) => b - a);
+  console.log(sort);
+  return arr.map((el) => sort.indexOf(el) + 1);
+};
+
+const solution = (arr) => {
+  let n = arr.length;
+  let answer = Array.from({ length: n }, () => 1);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i] < arr[j]) answer[i]++;
+    }
+  }
+
+  return answer;
+};
+
+let arr = [87, 89, 92, 92, 100, 76];
+console.log("me", mySolution(arr));
+console.log("lecture", solution(arr));
+
 // 문6)
 // 문7)
