d6238c270309ce75f6b582a1a957acab1d9486ee
yenicall <yenicall114@gmail.com>
2023-01-16 15:49:54 +0900

    s1-13

--- s1.js
+++ s1.js
@@ -424,7 +424,6 @@ console.log("lecture", solution(str));
 ItisTimeToStudy
 ▣ 출력예제 1 
 ITISTIMETOSTUDY
-*/
 
 const mySolution = (str) => {
   // let answer = "";
@@ -451,5 +450,41 @@ const solution = (s) => {
 };
 
 let str = "ItisTimeToStudy";
+console.log("me", mySolution(str));
+console.log("lecture", solution(str));
+
+문제13) 대소문자 변환
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+▣ 입력설명
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+▣ 출력설명
+첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다.
+▣ 입력예제 1 StuDY
+▣ 출력예제 1 sTUdy
+*/
+
+const mySolution = (s) => {
+  let answer = "";
+
+  for (let x of s) {
+    let num = x.charCodeAt();
+    if (num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
+    else answer += String.fromCharCode(num + 32);
+  }
+  return answer;
+};
+
+const solution = (s) => {
+  let answer = "";
+
+  for (let x of s) {
+    if (x === x.toUpperCase()) answer += x.toLowerCase();
+    else answer += x.toUpperCase();
+  }
+
+  return answer;
+};
+
+let str = "StuDY";
 console.log("me", mySolution(str));
 console.log("lecture", solution(str));
