d598c0249ab91b11c2705008fae152de9d54bf03
yenicall <yenicall114@gmail.com>
2023-01-16 16:47:51 +0900

    s1-16

--- s1.js
+++ s1.js
@@ -539,7 +539,6 @@ console.log("lecture", solution(arr));
 ▣ 출력예제 1 u
 ▣ 입력예제 2 good
 ▣ 출력예제 2 oo
-*/
 
 const mySolution = (s) => {
   return s.length % 2 === 0
@@ -558,5 +557,34 @@ const solution = (s) => {
 };
 
 let str = "good";
+console.log("me", mySolution(str));
+console.log("lecture", solution(str));
+
+문제16) 중복문자제거
+소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하 세요.
+제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+▣ 입력설명
+첫 줄에 문자열이 입력됩니다.
+▣ 출력설명
+첫 줄에 중복문자가 제거된 문자열을 출력합니다.
+▣ 입력예제 1 ksekkset
+▣ 출력예제 1 kset
+*/
+
+const mySolution = (s) => {
+  return Array.from(new Set(s)).join("");
+};
+
+const solution = (s) => {
+  let answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === i) answer += s[i];
+  }
+
+  return answer;
+};
+
+let str = "ksekkset";
 console.log("me", mySolution(str));
 console.log("lecture", solution(str));
