7e877128845105f9e4145e3d443f34bd43286909
yenicall <yenicall114@gmail.com>
2023-01-16 16:30:32 +0900

    s1-15

--- s1.js
+++ s1.js
@@ -504,7 +504,6 @@ beautiful
 good
 ▣ 출력예제 1 
 beautiful
-*/
 
 const mySolution = (arr) => {
   const length = arr.map((el) => el.length);
@@ -529,3 +528,35 @@ const solution = (s) => {
 let arr = ["teacher", "time", "student", "beautiful", "good"];
 console.log("me", mySolution(arr));
 console.log("lecture", solution(arr));
+
+문제15) 가운데 문자 출력
+소문자로 된 단어(문자열)가 입력되면 그 단어의 가운데 문자를 출력하는 프로그램을 작성하세 요. 단 단어의 길이가 짝수일 경우 가운데 2개의 문자를 출력합니다.
+▣ 입력설명
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+▣ 출력설명
+첫 줄에 가운데 문자를 출력합니다.
+▣ 입력예제 1 study
+▣ 출력예제 1 u
+▣ 입력예제 2 good
+▣ 출력예제 2 oo
+*/
+
+const mySolution = (s) => {
+  return s.length % 2 === 0
+    ? s.substring(s.length / 2 - 1, s.length / 2 + 1)
+    : s[Math.floor(s.length / 2)];
+};
+
+const solution = (s) => {
+  let answer = "";
+  let mid = Math.floor(s.length / 2);
+
+  if (s.length % 2 === 1) answer = s.substr(mid, 1);
+  else answer = s.substr(mid - 1, 2);
+
+  return answer;
+};
+
+let str = "good";
+console.log("me", mySolution(str));
+console.log("lecture", solution(str));
