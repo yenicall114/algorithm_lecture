71e1b7a185b88d6233c2c011b5cda76185beb729
yenicall <yenicall114@gmail.com>
2023-01-17 21:30:51 +0900

    s2-07

--- s2.js
+++ s2.js
@@ -225,7 +225,6 @@ N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선�
 [19, 13, 30, 13, 19]]
 ▣ 출력예제 1 
 155
-*/
 
 const mySolution = (arr) => {
   let result = 0;
@@ -287,4 +286,96 @@ let arr = [
 console.log("me", mySolution(arr));
 console.log("lecture", solution(arr));
 
-// 문7)
+// 문7) 봉우리
+지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다. 각 격자 판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
+격자의 가장자리는 0으로 초기화 되었다고 가정한다.
+만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
+▣ 입력설명
+첫 줄에 자연수 N이 주어진다.(1<=N<=50)
+두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는 다.
+▣ 출력설명
+봉우리의 개수를 출력하세요.
+▣ 입력예제 1 
+5
+[[5,3,7,2,3], 
+[3,7,1,6,1], 
+[7,2,5,3,4], 
+[4,3,6,4,1], 
+[8,7,3,5,2]]
+▣ 출력예제 1 
+10
+*/
+
+const mySolution = (arr) => {
+  let result = [];
+
+  let x = [-1, 0, 1, 0];
+  let y = [0, 1, 0, -1];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      let check = 1;
+
+      for (let k = 0; k < x.length; k++) {
+        let row = i + x[k];
+        let column = j + y[k];
+
+        if (
+          row >= 0 &&
+          row < arr.length &&
+          column >= 0 &&
+          column < arr.length &&
+          arr[row][column] >= arr[i][j]
+        ) {
+          // !!row와 column이 존재하지 않는 행열에 가로막히는 상태를 선두로 지정해줘야 TypeError가 발생하지 않는다!!
+          check = 0;
+          break;
+        }
+      }
+      if (check) result.push(arr[i][j]);
+    }
+  }
+
+  return result.length;
+};
+
+const solution = (arr) => {
+  let answer = 0;
+
+  let n = arr.length;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        if (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < n &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) answer++;
+    }
+  }
+
+  return answer;
+};
+
+let arr = [
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+];
+console.log("me", mySolution(arr));
+console.log("lecture", solution(arr));
