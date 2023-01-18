7a42ca5e45e639246e9a44588d3caa6d66f4cbb0
yenicall <yenicall114@gmail.com>
2023-01-18 23:14:18 +0900

    s3-04

--- s3.js
+++ s3.js
@@ -116,7 +116,6 @@ console.log("lecture", solution(str));
 첫 번째 줄에 각 문자열 s의 각 문자가 문자 t와 떨어진 거리를 순서대로 출력한다.
 ▣ 입력예제 1 teachermode e
 ▣ 출력예제 1 10121012210
-*/
 
 const mySolution = (str, e) => {
   let arr1 = [];
@@ -186,4 +185,54 @@ let e = "e";
 console.log("me", mySolution(str, e));
 console.log("lecture", solution(str, e));
 
-// 문4)
+// 문4) 문자열 압축
+알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시 오. 단 반복횟수가 1인 경우 생략합니다.
+▣ 입력설명
+첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+▣ 출력설명
+첫 줄에 압축된 문자열을 출력한다.
+▣ 입력예제 1 KKHSSSSSSSE
+▣ 출력예제 1 K2HS7E
+*/
+
+const mySolution = (str) => {
+  let arr = [...str];
+  let cnt = 1;
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) cnt++;
+
+    if (arr[i] !== arr[i + 1]) {
+      result.push(arr[i]);
+
+      if (cnt > 1) result.push(cnt);
+
+      cnt = 1;
+    }
+  }
+
+  return result.join("");
+};
+
+const solution = (s) => {
+  let answer = "";
+  let cnt = 1;
+  s = s + " ";
+
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) cnt++;
+    else {
+      answer += s[i];
+      if (cnt > 1) answer += String(cnt);
+      cnt = 1;
+    }
+  }
+
+  return answer;
+};
+
+let str = "KKHSSSSSSSE";
+console.log("me", mySolution(str));
+console.log("lecture", solution(str));
